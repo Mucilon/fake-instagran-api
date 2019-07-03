85da34a420fc80c625aba54b7a5534dbde1d1df9
mucilon <joao.g.n91@gmail.com>
2019-07-02 22:20:11 -0300

    introduce graphql to project

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,7 +8,8 @@ import Routes from './routes'
 import Handlers from './resposeHandlers'
 import path from 'path'
 import cors from 'cors'
-
+import graphqlHTTP from 'express-graphql'
+import { createSchema } from '../graphql/createSchema'
 // class responsible for setting up and starting routes the API
 
 class Api {
@@ -19,14 +20,18 @@ class Api {
       this.middleware()
     }
 
-    middleware (): void{
+    async middleware () {
       this.express.use(morgan('dev'))
       this.express.use(cors())
       this.express.use(bodyParser.urlencoded({ extended: true }))
       this.express.use(bodyParser.json())
       this.express.use(Handlers.errorHandlerApi)
       this.express.use('/files', express.static(path.resolve(__dirname, '..', '..', 'uploads', 'resized')))
-
+      const schema = await createSchema()
+      this.express.use('/graphql', graphqlHTTP({
+        schema,
+        graphiql: true
+      }))
       this.router(this.express)
     }
 
